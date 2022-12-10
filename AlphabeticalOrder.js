2c95300ca08431cb0d49ba63d082dc61bde0e906
SAYANTAN DAS <111352163+DASSAYANTAN@users.noreply.github.com>
2022-12-10 09:37:57 +0530

    Update AlphabeticalOrder.js

--- AlphabeticalOrder.js
+++ AlphabeticalOrder.js
@@ -1,31 +1,7 @@
 //!convert letter of a given string in alphabetical order
 
-// !example 1
-function alphabet_Soup(str) {
-  return str.split("").sort().join("");
+function a(str) {
+    return str.split("").sort().join("");
 }
 
-console.log(alphabet_Soup("Python"));
-
-console.log(alphabet_Soup("Exercises"));
-
-// !example 2
-
-// let str = "helloworld";
-// let string_sort = str => {
-//   var i = 0,
-//     j;
-//   while (i < str.length) {
-//     j = i + 1;
-//     while (j < str.length) {
-//       if (str[j] < str[i]) {
-//         var temp = str[i];
-//         str[i] = str[j];
-//         str[j] = temp;
-//       }
-//       j++;
-//     }
-//     i++;
-//   }
-// };
-// console.log(string_sort("helloworld"));
+console.log(a(prompt("enter your name:")));
